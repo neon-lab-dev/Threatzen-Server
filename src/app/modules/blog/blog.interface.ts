eec206479f5ff6fd380d06889f62f4fb7ea5cc87
rahulsd380 <rahulsd380@gmail.com>
2026-09-21 16:33:02 +0600

    first commit

---
+++ src/app/modules/blog/blog.interface.ts
@@ -0,0 +1,11 @@
+export type TBlog = {
+  imageUrl: string;
+  title: string;
+  slug: string;
+  category: string;
+  shortDescription: string;
+  readTime: string;
+  content: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+};
---
+++ src/app/modules/blog/blog.model.ts
@@ -0,0 +1,57 @@
+import { Schema, model } from "mongoose";
+import type { TBlog } from "./blog.interface";
+import slugify from "slugify";
+
+const BlogSchema = new Schema<TBlog>(
+  {
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    slug: {
+      type: String,
+      unique: true,
+    },
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    readTime: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    shortDescription: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// ✅ HOOK MUST COME BEFORE MODEL CREATION
+BlogSchema.pre("save", function (next) {
+  if (this.isModified("title")) {
+    this.slug = slugify(this.title, {
+      lower: true,
+      strict: true,
+      trim: true,
+    });
+  }
+  next();
+});
+
+const Blog = model<TBlog>("Blog", BlogSchema);
+
+export default Blog;
