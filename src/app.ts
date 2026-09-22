4e485bac451bc27441a65a1aa91213e12bcc794d
rahulsd380 <rahulsd380@gmail.com>
2026-09-22 11:38:00 +0600

    Fixed

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 // app.use(express.static("./uploads"));
-app.use(cors({ origin: ['http://localhost:5173', 'https://threatzen.in'], credentials: true }));
+app.use(cors({ origin: ['http://localhost:5173', 'https://threatzen.in', 'https://www.threatzen.in'], credentials: true }));
 
 // Root route
 app.get('/', (req, res) => {
